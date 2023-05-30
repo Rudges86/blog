4f2718ce9969a2fa9f51515964621ac9d91b7fc0
Rudge Santos <rudges86@gmail.com>
2023-05-29 22:16:17 -0300

    Projeto concluido

---
+++ src/app/data/mock.ts
@@ -0,0 +1,35 @@
+export const mock = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    "materia":"Durante sua apresentação bimestral para acionistas, a Marvel anunciou que as filmagens de Homem de Ferro, seu primeiro filme com produção própria, começarão dia 12 de março em Los Angeles. Sob a direção de Jon Favreau, a adaptação dos quadrinhos com Robert Downey Jr., Terrence Howard, Gwyneth Paltrow e Jeff Bridges, estreará em 2 de maio de 2008.A empresa aproveitou também para revelar quais serão seus próximos filmes, depois de Homem de Ferro e O Incrível Hulk. Com lançamento a partir de 2009 estão previstos Capitão América, Nick Fury, Homem-Formiga, Vingadores e Thor.",
+    "link": "https://omelete.com.br/homem-formiga/inicio-de-homem-de-ferro-marcado-e-marvel-anuncia-proximos-filmes"
+
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Nova série da Feiticeira Escarlate é confirmada na Disney+",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+    "materia":"Introduzida no cinema através do trabalho da atriz Elizabeth Olsen, não custou tanto para que a Feiticeira Escarlate viesse a se tornar uma das super-heroínas mais populares de toda...",
+    "link":"https://legadodamarvel.com.br/nova-serie-da-feiticeira-escarlate-e-confirmada-na-disney/"
+  },
+  {
+    "id":"3",
+    "title": "Novo filme do pantera negra lançado em breve",
+    "description": "Pantera Negra faz história e se torna o primeiro filme do MCU a ganhar um Oscar",
+    "photoCover":"https://sm.ign.com/t/ign_br/screenshot/default/blob_wa8p.960.jpg",
+    "materia":"Demorou mais de 10 anos, mas a Marvel Studios finalmente tem um Oscar para chamar de seu -- não só um, como três. Pantera Negra, o primeiro filme de super-herói indicado a Melhor Filme, fez história novamente e se tornou a primeira produção do MCU a levar para casa a estatueta mais cobiçada da indústria cinematográfica.",
+    "link":"https://br.ign.com/pantera-negra/71133/news/pantera-negra-faz-historia-e-se-torna-o-primeiro-filme-do-mcu-a-ganhar-um-oscar"
+  },
+  {
+    "id":"4",
+    "title": "Novo filme dos X-men está em pós produção, afirma diretor",
+    "description": "Produção é um dos principais desejos dos fãs da Marvel desde 2019, ano em que a Disney, detentora do universo de super-heróis, comprou a Fox.",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339",
+    "materia":"Após a música de abertura da animação ser reproduzida no final da série Ms.Marvel, o sinal de alerta dos fãs acendeu. Segundo Deadline, uma nova produção cinematográfica da Marvel está por vir. Intitulado Os mutantes, o filme deve ser anunciado durante a Comic Con 2022, convenção que acontece dos dias 21 a 24 de julho.",
+    "link":"https://www.correiobraziliense.com.br/diversao-e-arte/2022/07/5024043-x-men-mutantes-podem-ser-introduzidos-em-novo-filme-da-marvel.html"
+  }
+]
